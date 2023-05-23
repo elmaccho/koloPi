20865d975c7cc0f26add8590f669b29e57ba054b
elmaccho <maciek.chojnacki22@wp.pl>
2023-05-23 23:28:46 +0200

    about us section

--- script.js
+++ script.js
@@ -21,8 +21,60 @@ const navScroll = () => {
         nav.classList.remove('activeNav')
     }
 }
+// const navHighlight = () => {
+//     var navLinks = document.querySelectorAll(".nav_links a");
+//     var sections = document.querySelectorAll("section");
+  
+//     function highlightNavLink() {
+//       var scrollPosition = window.pageYOffset;
+  
+//       sections.forEach(function(section, index) {
+//         if (
+//           scrollPosition >= section.offsetTop &&
+//           scrollPosition < section.offsetTop + section.offsetHeight
+//         ) {
+//           navLinks.forEach(function(navLink) {
+//             navLink.classList.remove("active");
+//           });
+//           navLinks[index].classList.add("active");
+//         }
+//       });
+//     }
+  
+//     window.addEventListener("scroll", highlightNavLink);
+// }
 
 closeNavBtn.addEventListener('click', closeNavMen)
 openNavBtn.addEventListener('click', openNavMen)
 document.addEventListener('click', outsideNav)
 window.addEventListener('scroll', navScroll)
+// document.addEventListener("scroll", navHighlight);
+
+document.addEventListener("DOMContentLoaded", function() {
+    var navLinks = document.querySelectorAll(".nav_links a");
+    var sections = document.querySelectorAll("section");
+  
+    function highlightNavLink() {
+      var scrollPosition = window.pageYOffset;
+  
+      sections.forEach(function(section) {
+        var sectionTop = section.offsetTop;
+        var sectionHeight = section.offsetHeight;
+        var sectionId = section.getAttribute("id");
+        var navLink = document.querySelector('a[href="#' + sectionId + '"]');
+  
+        if (
+          scrollPosition >= sectionTop &&
+          scrollPosition < sectionTop + sectionHeight
+        ) {
+          navLinks.forEach(function(link) {
+            link.classList.remove("active");
+          });
+          navLink.classList.add("active");
+        }
+      });
+    }
+  
+    window.addEventListener("scroll", highlightNavLink);
+  });
+  
